7db94bac63bcc38b4627123fc93aa8c88537fcf7
satomi-kuwahara1931 <satomi-kuwahara@hiroba1931.co.jp>
2026-01-28 14:13:16 +0900

    OpenAPI config update

--- src/api/core/OpenAPI.ts
+++ src/api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: '',
+    BASE: import.meta.env.VITE_API_BASE_URL,
     VERSION: '1.0.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
